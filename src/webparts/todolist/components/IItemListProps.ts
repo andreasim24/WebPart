4d3ab07bd3f49b252f4e850a758087c30dad72d7
andreas <andreasim2426@gmail.com>
2022-11-22 13:19:07 +0700

    fix remarks
    - change HelloWorld to ToDoList
    - remove commented block code
    - change styling issue
    - move ListItem to separate service class
    - use $select=Title,Status for retrieving specific fields
    - handle error message
    - use localization instead hard code
    - change ternary operator in className to Switch Case Operator

---
+++ src/webparts/todolist/components/IItemListProps.ts
@@ -0,0 +1,5 @@
+import { SPHttpClient } from "@microsoft/sp-http";
+
+export interface IItemListProps {
+  spHttpClient: SPHttpClient;
+}
---
+++ src/webparts/todolist/components/ToDoList.tsx
@@ -0,0 +1,126 @@
+import * as React from "react";
+import styles from "./ToDoList.module.scss";
+import { IToDoListProps } from "./IToDoListProps";
+import { IItemListProps } from "./IItemListProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { SPHttpClient } from "@microsoft/sp-http";
+import { Icon, List } from "office-ui-fabric-react";
+import * as strings from "ToDoListWebPartStrings";
+
+export interface ISPListItem {
+  Title: string;
+  Id: string;
+  Status: string;
+}
+
+export interface IListItemState {
+  items: ISPListItem[];
+  errorMessage: any;
+}
+
+class ItemList extends React.Component<IItemListProps, IListItemState> {
+  constructor(props: IItemListProps, state: IListItemState) {
+    super(props);
+
+    this.state = {
+      items: [],
+      errorMessage: null
+    };
+  }
+
+  private async _getListData(): Promise<ISPListItem[]> {
+    try {
+      const response = await this.props.spHttpClient.get(
+        "https://xq0nb.sharepoint.com/sites/TestSite/_api/web/lists/getByTitle('To do list')/items?$select=Title,Status",
+        SPHttpClient.configurations.v1
+      );
+
+      if (!response.ok) {
+        const responseText = await response.text();
+        throw new Error(responseText);
+      }
+
+      const data = await response.json();
+
+      this.setState({ items: data.value });
+      return data.value;
+    } catch (error) {
+      this.setState({ errorMessage: error.message });
+    }
+  }
+
+  public componentDidMount(): void {
+    this._getListData();
+  }
+
+  public render(): React.ReactElement<IItemListProps> {
+    return (
+      <>
+        <List items={this.state.items} onRenderCell={this._onRenderListItem} />
+        {this.state.errorMessage && <span>{this.state.errorMessage}</span>}
+      </>
+    );
+  }
+
+  public _itemStatus = (status: string): string => {
+    switch (status) {
+      case "Pending":
+        return styles.itemPending;
+
+      case "Completed":
+        return styles.itemCompleted;
+
+      case "Active":
+        return styles.itemActive;
+
+      case "Overdue":
+        return styles.itemOverdue;
+
+      default:
+        return styles.itemStatus;
+    }
+  };
+
+  public _onRenderListItem = (
+    item: ISPListItem,
+    index: number
+  ): JSX.Element => {
+    return (
+      <div key={index} data-is-focusable={true}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <span>{item.Title}</span>
+            <span
+              className={`${styles.itemStatus} ${this._itemStatus(
+                item.Status
+              )}`}
+            >
+              {item.Status}
+              <Icon
+                className={styles.itemIcon}
+                iconName={`${item.Status === "Completed" ? "Completed" : null}`}
+              />
+            </span>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+}
+
+export default class ToDoList extends React.Component<IToDoListProps, {}> {
+  public render(): React.ReactElement<IToDoListProps> {
+    const { hasTeamsContext, userDisplayName } = this.props;
+
+    return (
+      <section
+        className={`${styles.todoList} ${hasTeamsContext ? styles.teams : ""}`}
+      >
+        <h2>
+          {strings.ToDoListHeading} {escape(userDisplayName)}
+        </h2>
+        <ItemList spHttpClient={this.props.spHttpClient} />
+      </section>
+    );
+  }
+}
